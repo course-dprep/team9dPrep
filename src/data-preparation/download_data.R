de1b966560ceeba7477b35a32ac788d4a3d9efa0
Tommaso Rabino <t.rabino@tilburguniversity.edu>
2023-09-08 16:06:10 +0200

    changes in the download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,4 +1,21 @@
 # Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('http://data.insideairbnb.com/italy/lombardy/milan/2023-06-21/data/listings.csv.gz','../../data/listings_milan_.csv.gz')
+
+# Define the URL and the target file name
+url <- "http://data.insideairbnb.com/italy/lombardy/milan/2023-06-21/data/listings.csv.gz"
+target_file <- "../../data/listings_milan.csv.gz"  # Relative path to the data directory
+
+# Use download.file to download the file
+download.file(url, target_file, method = "auto")
+
+# Define the absolute path to the zip folder and the target CSV file
+zip_file <- file.path(getwd(), "../../data/listings_milan.csv.gz")
+csv_file <- file.path(getwd(), "../../data/listings.csv")
+
+# Unzip the contents of the zip folder
+unzip(zip_file, exdir = csv_file)
+
+
+# Load the extracted CSV file into R
+data <- read.csv(csv_file)
+
 
