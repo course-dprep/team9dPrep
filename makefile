3b3fa826393c440cb91c99f8d690982ca4e61b84
Tommaso Rabino <t.rabino@tilburguniversity.edu>
2023-10-04 10:56:39 +0200

    makefiles

--- makefile
+++ makefile
@@ -1,45 +1,10 @@
-# Define variables
-R_SCRIPT := src/data-preparation/0_installing_packages.R
-RMD_FILES := src/data-preparation/1_data_download.Rmd \
-             src/data-preparation/2_data_cleaning.Rmd \
-             src/data-preparation/3_data_exploration.Rmd \
-             src/data-preparation/4_data_preparation.Rmd \
-             src/analysis/5_regression_model.Rmd
+# Makefile in the main directory
 
-R_FILES := src/analysis/6_shinyapp.R \
-           src/analysis/7_clean-up.R
+all: data-preparation analysis
 
-ZIP_FOLDER := gen/paper/output/final_paper
-MAIN_TEX_FILE := $(ZIP_FOLDER)/main.tex
-PDF_OUTPUT := $(ZIP_FOLDER)/main.pdf
+data-preparation:
+	make -C src/data-preparation
 
-# Define targets
-all: install_packages data_analysis run_shinyapp clean_up assemble_pdf
+analysis: data-preparation
+	make -C src/analysis
 
-# Target to install R packages
-install_packages:
-    Rscript $(R_SCRIPT)
-
-# Rule to generate PDF from Rmd
-%.pdf: %.Rmd
-    Rscript -e "rmarkdown::render('$<', output_format = 'pdf_document')"
-
-# Target for data analysis
-data_analysis: $(RMD_FILES:.Rmd=.pdf)
-
-# Target to run Shiny app
-run_shinyapp: $(R_FILES)
-    Rscript $<
-
-# Target to clean up
-clean_up: src/analysis/7_clean-up.R
-    Rscript $<
-
-# Target to assemble PDF from LaTeX files
-assemble_pdf: $(PDF_OUTPUT)
-
-# Rule to generate PDF from LaTeX files
-$(PDF_OUTPUT): $(MAIN_TEX_FILE) $(ZIP_FOLDER)/LTJournalArticle.cls $(ZIP_FOLDER)/sample.bib $(ZIP_FOLDER)/Figures/*.png
-    cd $(ZIP_FOLDER) && pdflatex main.tex && bibtex main && pdflatex main.tex && pdflatex main.tex
-
-	
---
+++ src/data-preparation/clean-up.R
@@ -0,0 +1,52 @@
+#GENERAL PACKAGES:
+library(readr)
+library(tidyverse) #A "Package of Packages" for Data manipulation and visualization (includes magrittr, lubridate, purrr, tidyr, etc.).
+library(dplyr) #Data frame manipulations (select, slice, etc.
+library(jsonlite) #For Amenities Columns Creation
+library(moments) #Measuring the skewness.
+
+#REGRESSION PACKAGES
+library(caret) #Hyperparameters Tuning. 
+library(xgboost) #XGBoost Regression. 
+library(DALEX) #Summary of the XGBoost Regression Model ("explainer).
+library(bayesforecast) #Checking Regression Assumptions.
+
+#SHINYAPP PACKAGES
+library(shiny) #For the ShinyApp
+library(shinyWidgets) #For the ShinyApp
+
+#PLOT AND FIGURES PACKAGES:
+library(ggplot2) #Building fancy plots.
+library(ggthemes) #Themes for ggplots (e.g. "solarized").
+library(ggcorrplot) #For correlograms
+library(scales) #Scaling and formatting ggplots (e.g. scale_fill_gradient()).
+library(gt) #Latex tables
+
+# WORKING DIRECTORY SETTING PACKAGES
+library(here)
+library(rstudioapi)
+
+
+
+# Get the path of the active document
+current_document_path <- getActiveDocumentContext()$path
+
+# Check if the document path is set
+if (!is.null(current_document_path)) {
+  # Set the working directory to the source file's location
+  setwd(dirname(current_document_path))
+  
+  # Optionally, you can remove the document path variable
+  rm(current_document_path)
+} else {
+  cat("No active document or path found. Set working directory manually.\n")
+}
+
+
+
+# Deletes files in all subdirectories with the endings specified here
+fileEndings <- c('*.log','*.aux','*.Rout','*.Rhistory','*.fls','*.fdb_latexmk', ".RData")
+for (fi in fileEndings) { 
+  files <- list.files(getwd(),fi,include.dirs=F,recursive=T,full.names=T,all.files=T)
+  file.remove(files)
+}
