9208f65281a4ebb2b7cd0e4fb55a2a7e617daf80
Tommaso Rabino <t.rabino@tilburguniversity.edu>
2023-10-05 11:36:01 +0200

    XXX

--- src/data-preparation/0_installing_packages.R
+++ src/data-preparation/0_installing_packages.R
@@ -110,18 +110,24 @@ library(rstudioapi)
 ######      AUTOMATICALLY SET WORKING DIRECTORY         #######
 ###############################################################
 
-# Get the path of the active document
-current_document_path <- getActiveDocumentContext()$path
-
-# Check if the document path is set
-if (!is.null(current_document_path)) {
-  # Set the working directory to the source file's location
-  setwd(dirname(current_document_path))
+if (rstudioapi::isAvailable()) {
+  # Get the path of the active document
+  current_document_path <- getActiveDocumentContext()$path
   
-  # Optionally, you can remove the document path variable
-  rm(current_document_path)
+  # Check if the document path is set
+  if (!is.null(current_document_path)) {
+    # Set the working directory to the source file's location
+    setwd(dirname(current_document_path))
+    
+    # Optionally, you can remove the document path variable
+    rm(current_document_path)
+  } else {
+    cat("No active document or path found. Set working directory manually.\n")
+  }
 } else {
-  cat("No active document or path found. Set working directory manually.\n")
+  # This section is executed when not in RStudio
+  # Here, you can set the working directory manually or leave it at its default
+  cat("Script is not running in RStudio. Consider setting the working directory manually if required.\n")
 }
 
 
