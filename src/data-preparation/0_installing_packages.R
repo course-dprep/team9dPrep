a8307d12292927dccbc7c83b54a76dbb5fd0f046
Tommaso Rabino <t.rabino@tilburguniversity.edu>
2023-10-08 18:11:21 +0200

    last

--- src/data-preparation/0_installing_packages.R
+++ src/data-preparation/0_installing_packages.R
@@ -71,30 +71,6 @@ cat('\nDone!\n\n')
 #############           LOAD PACKAGES            ##############
 ###############################################################
 
-#GENERAL PACKAGES:
-library(readr)
-library(tidyverse) #A "Package of Packages" for Data manipulation and visualization (includes magrittr, lubridate, purrr, tidyr, etc.).
-library(dplyr) #Data frame manipulations (select, slice, etc.
-library(jsonlite) #For Amenities Columns Creation
-library(moments) #Measuring the skewness.
-
-#REGRESSION PACKAGES
-library(caret) #Hyperparameters Tuning. 
-library(xgboost) #XGBoost Regression. 
-library(DALEX) #Summary of the XGBoost Regression Model ("explainer).
-library(bayesforecast) #Checking Regression Assumptions.
-
-#SHINYAPP PACKAGES
-library(shiny) #For the ShinyApp
-library(shinyWidgets) #For the ShinyApp
-
-#PLOT AND FIGURES PACKAGES:
-library(ggplot2) #Building fancy plots.
-library(ggthemes) #Themes for ggplots (e.g. "solarized").
-library(ggcorrplot) #For correlograms
-library(scales) #Scaling and formatting ggplots (e.g. scale_fill_gradient()).
-library(gt) #Latex tables
-
 # WORKING DIRECTORY SETTING PACKAGES
 library(here)
 library(rstudioapi)
